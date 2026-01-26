a73cca0af9e9a289960022b9c837c930ee3040ce
Gehad Sweeney <gehadsowany2001@gmail.com>
2026-01-26 23:52:35 +0200

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
 return (
     <div className="header">
-        <img src="./header.png" alt="Header banner featuring bags catalog branding and navigation elements" width="100%"/>
+        <img src="./banner.png" alt="Header banner featuring bags catalog branding and navigation elements" width="100%"/>
     </div>
 )
 }
