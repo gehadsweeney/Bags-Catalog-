fcd078620518b6f7cf5e71319bb0a48c1c5e6bf0
Gehad Sweeney <gehadsowany2001@gmail.com>
2026-02-08 01:03:28 +0200

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import './Header.css'
 
 const Header = () => {
 return (
     <div className="header">
-        <img src="./banner.png" alt="Header banner featuring bags catalog branding and navigation elements" width="100%"/>
+        <img src="/banar.png" alt="Header banner featuring bags catalog branding and navigation elements" width="100%"/>
+        <div className="content">
+            <a className="whatsapp-button" href="https://wa.me/+966503863911" target="_blank" rel="noreferrer">
+                <img className='button' src="/button.png" alt="WhatsApp contact button"/>
+            </a>
+            <img src="/wats.png" alt="WhatsApp icon for contact or support" />
+        </div>
     </div>
 )
 }
